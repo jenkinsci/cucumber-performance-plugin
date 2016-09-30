674c3d0657feb18d01d3f8b198f4a786d3999e35
Brian Riddle <brian.riddle@avanza.se>
2016-09-30 12:48:03 +0200

    Updated syntax jQuery() -> $()

--- src/main/webapp/javascript/scenarioSortedTable.js
+++ src/main/webapp/javascript/scenarioSortedTable.js
@@ -1,6 +1,6 @@
-jQuery(document).ready(function() {
+$(document).ready(function() {
 	/* Scenario-level table */
-	jQuery('#sortedScenarioTable').dataTable({
+	$('#sortedScenarioTable').dataTable({
 		"aoColumns" : [ {
 			"sType" : "string"
 		}, null, null, {
